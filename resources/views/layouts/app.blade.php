12070f3cce27a945f7ac47a1051a9ea7dfdd59eb
Almita05 <martinezlanderoalmalilia@gmail.com>
2026-04-25 18:38:46 -0600

    logo en pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
 <meta charset="UTF-8">
 <title>BTI</title>
+<link rel="icon" href="{{ asset('img/logo.png') }}" type="image/png">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
